a2aefbed4213ab2ecbcdaf2eca8fe64fb594f7ec
ErickSalazar07 <ericksalazar@javeriana.edu.co>
2025-02-22 16:13:39 -0500

    fix(): Se organizo el proyecto en base al framework Spring Boot

---
+++ veterinaria/src/main/java/puj/veterinaria/controladores/ControladorPrincipal.java
@@ -0,0 +1,20 @@
+package puj.veterinaria.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class ControladorPrincipal {
+
+  @GetMapping("")
+  public String paginaPrincipal() {
+    return "html/index";
+  }
+
+  @GetMapping("index.html")
+  public String paginaIndex() {
+    return "html/index";
+  }
+}
