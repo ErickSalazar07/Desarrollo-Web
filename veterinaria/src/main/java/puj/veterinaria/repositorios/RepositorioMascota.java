ea673cc6cfccfa1d9f68f72e19e03730d08d90f8
ErickSalazar07 <ericksalazar@javeriana.edu.co>
2025-02-24 22:07:29 -0500

    fix(RepositorioMascota.java): Se agrego una nueva mascota al repositorio

--- veterinaria/src/main/java/puj/veterinaria/repositorios/RepositorioMascota.java
+++ veterinaria/src/main/java/puj/veterinaria/repositorios/RepositorioMascota.java
@@ -13,12 +13,14 @@ public class RepositorioMascota {
   Map<Integer,Mascota> baseDatosMascotas;
   public RepositorioMascota() {
     baseDatosMascotas = new HashMap<>();
-    baseDatosMascotas.put(1, new Mascota("Pachini","Gato",5,4.5,null,
+    baseDatosMascotas.put(1, new Mascota("Pachini","Gato",5,4.5,"VIF",
     "https://github.com/ErickSalazar07/Web/blob/main/imagenes/mascotas/gato-pachini.jpg?raw=true",null));
     baseDatosMascotas.put(2, new Mascota("Zeus","Perro",9,11.7,null,
     "https://github.com/ErickSalazar07/Web/blob/main/imagenes/mascotas/perro-zeus.jpg?raw=true",null));
-    baseDatosMascotas.put(3, new Mascota("Figaro","Perro",9,19.7,null,
+    baseDatosMascotas.put(3, new Mascota("Figaro","Perro",9,19.7,"Cataratas",
     "https://github.com/ErickSalazar07/Web/blob/main/imagenes/mascotas/perro-figaro.jpg?raw=true",null));
+    baseDatosMascotas.put(4, new Mascota("Lola","Gato",3,6.0,null,
+    "https://github.com/ErickSalazar07/Web/blob/main/imagenes/mascotas/gato-lola.jpg?raw=true",null));
   }
 
   public Mascota findById(Integer id) {
