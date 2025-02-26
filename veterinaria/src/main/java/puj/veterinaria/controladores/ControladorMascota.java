78921250eb0877f5c36dddfb7e91302673f07daa
ErickSalazar07 <ericksalazar@javeriana.edu.co>
2025-02-26 08:31:41 -0500

    fix(ControladorMascota.java): Se arreglo la URL mostrar-mascota y mostrar-mascotas

--- veterinaria/src/main/java/puj/veterinaria/controladores/ControladorMascota.java
+++ veterinaria/src/main/java/puj/veterinaria/controladores/ControladorMascota.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import puj.veterinaria.servicios.IMascotaServicio;
 
@@ -35,12 +34,4 @@ public class ControladorMascota {
     modelo.addAttribute("mascota", mascotaServicio.searchById(id));
     return "html/mascota/mostrar-mascota";
   }
-
-  // // ? Cambiar id por algun id dentro de los animales guardados en el repositorio
-  // // URL: http://localhost:8090/mascota/mostrar-mascota?id=1 
-  // @GetMapping("/mostrar-mascota")
-  // public String mostrarMascota(Model modelo, @RequestParam("id") Integer id) {
-  //   modelo.addAttribute("mascota", mascotaServicio.searchById(id));
-  //   return "html/mascota/mostrar-mascota";
-  // }
 }
