af07e63fb578f601fe8c5db8603a5a9874ecc36a
Erick <ericksalazar@javeriana.edu.co>
2025-04-06 16:08:20 -0500

    feat: Se agrega ControladorDroga.java
    Se implementa el servicio controlador REST, el cual permite la realizacion de peticiones CRUD.

---
+++ veterinaria/src/main/java/puj/veterinaria/servicios/DrogaServicio.java
@@ -0,0 +1,54 @@
+package puj.veterinaria.servicios;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import puj.veterinaria.entidades.Droga;
+import puj.veterinaria.repositorios.RepositorioDroga;
+
+@Service
+public class DrogaServicio implements IDrogaServicio {
+
+  @Autowired
+  RepositorioDroga repositorioDroga;
+
+  @Override
+  public List<Droga> findAll() {
+    return repositorioDroga.findAll();
+  }
+
+  @Override
+  public Droga findById(Long id) {
+    return repositorioDroga.findById(id).orElse(null);
+  }
+
+  @Override
+  public void addDroga(Droga droga) {
+    repositorioDroga.save(droga);
+  }
+
+  // TODO: Falta implementar esta version de actualizar una Droga dado un id.
+  @Override
+  public void updateDroga(Long id, Droga droga) {
+    Droga drogaModificar = repositorioDroga.findById(id).orElse(null);
+
+    if(drogaModificar == null) return;
+  }
+
+  @Override
+  public void updateDroga(Droga droga) {
+    repositorioDroga.save(droga);
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    repositorioDroga.deleteById(id);
+  }
+
+  @Override
+  public Long numDrogas() {
+    return repositorioDroga.count();
+  }
+}
---
+++ veterinaria/src/main/java/puj/veterinaria/servicios/IDrogaServicio.java
@@ -0,0 +1,15 @@
+package puj.veterinaria.servicios;
+
+import java.util.List;
+
+import puj.veterinaria.entidades.Droga;
+
+public interface IDrogaServicio {
+  public List<Droga> findAll();
+  public Droga findById(Long id);
+  public void addDroga(Droga droga);
+  public void updateDroga(Long id, Droga droga);
+  public void updateDroga(Droga droga);
+  public void deleteById(Long id);
+  public Long numDrogas();
+}
