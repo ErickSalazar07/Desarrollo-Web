7dfdc09a11cde08d6fa9f9c889a11430ab6972a4
davidjcuadros <cuadrosdavid01@gmail.com>
2025-05-02 17:30:21 -0500

    Pruebas Querys MascotaRepositorio

--- veterinaria/src/test/java/puj/veterinaria/veterinaria/repository/MascotaRepositorioTest.java
+++ veterinaria/src/test/java/puj/veterinaria/veterinaria/repository/MascotaRepositorioTest.java
@@ -1,5 +1,76 @@
 package puj.veterinaria.veterinaria.repository;
 
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.assertj.core.api.Assertions;
+
+import puj.veterinaria.entidades.Cliente;
+import puj.veterinaria.entidades.Mascota;
+import puj.veterinaria.entidades.DTO.MascotaDTO;
+import puj.veterinaria.repositorios.RepositorioCliente;
+import puj.veterinaria.repositorios.RepositorioMascota;
+
+@DataJpaTest
+@RunWith(SpringRunner.class)
 public class MascotaRepositorioTest {
+
+    @Autowired
+    private RepositorioMascota repositorioMascota;
+
+    @Autowired
+    private RepositorioCliente repositorioCliente;
     
+    @BeforeEach
+    public void setup(){
+        Cliente cliente = new Cliente("12341234", "nuevo", "nuevo@nuevo", "7845628");
+
+        MascotaDTO mascotaDTO1 = new MascotaDTO(-1L,"nuevoMascota", "Perro", 12, 12.0, "x", "foto", false, "12341234");
+        Mascota mascota1 = new Mascota(mascotaDTO1);
+        mascota1.setCliente(cliente);
+
+        MascotaDTO mascotaDTO2 = new MascotaDTO(-1L,"nuevoMascota", "Perro", 12, 12.0, "x", "foto", false, "12341234");
+        Mascota mascota2 = new Mascota(mascotaDTO2);
+        mascota2.setCliente(cliente);
+
+        MascotaDTO mascotaDTO3 = new MascotaDTO(-1L,"nuevoMascota", "Perro", 12, 12.0, "x", "foto", true, "12341234");
+        Mascota mascota3 = new Mascota(mascotaDTO3);
+        mascota3.setCliente(cliente);
+
+        repositorioCliente.save(cliente);
+        repositorioMascota.save(mascota1);
+        repositorioMascota.save(mascota2);
+        repositorioMascota.save(mascota3);
+    }
+    //TEST QUERYS ------------------------------------------------------
+    @Test
+    public void mascotaRepositorio_cantidadMascotasActivas_long(){
+        //Arrange
+        Cliente cliente = new Cliente("12341234", "nuevo", "nuevo@nuevo", "7845628");
+
+        MascotaDTO mascotaDTO1 = new MascotaDTO(-1L,"nuevoMascota", "Perro", 12, 12.0, "x", "foto", false, "12341234");
+        Mascota mascota1 = new Mascota(mascotaDTO1);
+        mascota1.setCliente(cliente);
+
+        MascotaDTO mascotaDTO2 = new MascotaDTO(-1L,"nuevoMascota", "Perro", 12, 12.0, "x", "foto", false, "12341234");
+        Mascota mascota2 = new Mascota(mascotaDTO2);
+        mascota2.setCliente(cliente);
+
+        MascotaDTO mascotaDTO3 = new MascotaDTO(-1L,"nuevoMascota", "Perro", 12, 12.0, "x", "foto", true, "12341234");
+        Mascota mascota3 = new Mascota(mascotaDTO3);
+        mascota3.setCliente(cliente);
+
+        repositorioCliente.save(cliente);
+        repositorioMascota.save(mascota1);
+        repositorioMascota.save(mascota2);
+        repositorioMascota.save(mascota3);
+
+        //Act
+        Long cantidadMascotasActivas = repositorioMascota.cantidadMascotasActivas();
+        //Assert
+        Assertions.assertThat(cantidadMascotasActivas).isEqualTo(1);
+    }
 }
--- veterinaria/src/test/java/puj/veterinaria/veterinaria/repository/TratamientoRepositorioTest.java
+++ veterinaria/src/test/java/puj/veterinaria/veterinaria/repository/TratamientoRepositorioTest.java
@@ -1,5 +1,47 @@
 package puj.veterinaria.veterinaria.repository;
 
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import org.assertj.core.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import puj.veterinaria.entidades.Droga;
+import puj.veterinaria.entidades.Mascota;
+import puj.veterinaria.entidades.Tratamiento;
+import puj.veterinaria.entidades.Veterinario;
+import puj.veterinaria.entidades.DTO.TratamientoDTO;
+import puj.veterinaria.repositorios.RepositorioTratamiento;
+
+@DataJpaTest
+@RunWith(SpringRunner.class)
 public class TratamientoRepositorioTest {
+
+    @Autowired
+    private RepositorioTratamiento repositorioTratamiento;
+
+    // @Test
+    // public void tratamientoRepositorio_cantidadTratamientosPorTipoMedicamento_long(){
+    //     //arrange
+    //     Droga drogaPrueba = new Droga("drogaPrueba", 1.0, 1.0, 3, 0);
+    //     Veterinario veterinario1 = new Veterinario("12343187634534", "nuevoVeterinario", "1234","nuevaEspecialidad", "foto", true);
+    //     Mascota mascota1 = new Mascota("nuevoMascota", "Perro", 12, 12.0, "x", "foto", true);
+        
+    //     TratamientoDTO tratamientoDTO = new TratamientoDTO(12345L, "TratamientoPrueba", LocalDate.now(), Long drogaAsignadaID, Long mascotaID,
+    //   "12343187634534");
+    //     Tratamiento tratamiento1 = new Tratamiento();
+    //     Tratamiento tratamiento2 = new Tratamiento();
+    
+    //     repositorioTratamiento.save(tratamiento1);
+    //     //act
+    //     long cantidadTratamientosPorTipoMedicamento = repositorioTratamiento.cantidadTratamientosPorTipoMedicamento("drogaPrueba");
+    //     //assert
+    //     Assertions.assertThat(cantidadTratamientosPorTipoMedicamento).isEqualTo(1);
+
+    //}
     
 }
