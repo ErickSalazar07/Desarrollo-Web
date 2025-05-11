01c48cf9ab44b5c52f222341837b7f3b57e8bed8
Erick <ericksalazar@javeriana.edu.co>
2025-05-10 22:33:16 -0500

    feat: Se agrega caso 2 testing e2e

--- veterinaria/src/test/java/puj/veterinaria/veterinaria/e2e/VeterinarioCaso2.java
+++ veterinaria/src/test/java/puj/veterinaria/veterinaria/e2e/VeterinarioCaso2.java
@@ -4,7 +4,10 @@ import java.time.Duration;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -40,7 +43,64 @@ public class VeterinarioCaso2 {
 
   @Test
   public void SystemTest_VeterinarioCaso2_agregarDroga() {
-    driver.get("localhost:4200/veterinario/login");
+    // Se calculan valores iniciales desde las kpis del admin
+    Double gananciasIniciales = gananciasTotales();
+    Long tratamientosIniciales = tratamientosTotales();
+  
+    // Suministrar medicamento
+  
+    // Se calculan valores finales despues de suministrar medicamento tambien desde kpis
+    Double gananciasFinales = gananciasTotales();
+    Long tratamientosFinales = tratamientosTotales();
+  }
+  
+  private void suministrarMedicamento() {
+    driver.get("http://localhost:4200/veterinario/dashboard/789/mascota/mascotas");
+
+    String cssSelector = "section.busqueda input";
+    WebElement inpTxtBuscar = driver.findElement(By.cssSelector(cssSelector));
+    inpTxtBuscar.sendKeys("Pachini");
   }
 
+  private Double gananciasTotales() {
+    // Seccion login
+    driver.get("http://localhost:4200/admin/login");
+    WebElement inpTxtUsername = driver.findElement(By.id("username"));
+    WebElement inpTxtCelular = driver.findElement(By.id("celular"));
+    WebElement btnIngresar = driver.findElement(By.cssSelector("form button"));
+
+    inpTxtUsername.sendKeys("admin");
+    inpTxtCelular.sendKeys("123");
+    btnIngresar.click();
+
+    // Seccion kpis
+    WebElement btnKpi = driver.findElement(By.cssSelector("div#kpis"));
+    btnKpi.click();
+
+    String selectPGanancia = "#seccion-ganancias p";
+    WebElement pGanancias = driver.findElement(By.cssSelector(selectPGanancia));
+    Double ganancias = Double.parseDouble(pGanancias.getText().replace(",","").substring(2));
+    return ganancias;
+  }
+  
+  private Long tratamientosTotales() {
+    // Seccion login
+    driver.get("http://localhost:4200/admin/login");
+    WebElement inpTxtUsername = driver.findElement(By.id("username"));
+    WebElement inpTxtCelular = driver.findElement(By.id("celular"));
+    WebElement btnIngresar = driver.findElement(By.cssSelector("form button"));
+
+    inpTxtUsername.sendKeys("admin");
+    inpTxtCelular.sendKeys("123");
+    btnIngresar.click();
+
+    // Seccion kpis
+    WebElement btnKpi = driver.findElement(By.cssSelector("div#kpis"));
+    btnKpi.click();
+
+    String selectPTratamientos = "#seccion-tratamientos p";
+    WebElement pTratamientos = driver.findElement(By.cssSelector(selectPTratamientos));
+    Long tratamientos = Long.parseLong(pTratamientos.getText());
+    return tratamientos;
+  }
 }
